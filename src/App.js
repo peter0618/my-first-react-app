97ef9122c5de16f887fcda0a95f9b6b93dd0b3fe
peter0618 <peterchoi0618@gmail.com>
2020-11-08 00:51:20 +0900

    컴포넌트 이벤트 생성 및 state 변경(setState)

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            mode:'welcome',
             subject:{title:'WEB', sub:'World Wide Web!'},
+            welcome:{title:'Welcome', desc:'Hello, React!!'},
             contents:[
                 {id:1, title:'HTML', desc:'HTML is for information'},
                 {id:2, title:'CSS', desc:'CSS is for design'},
@@ -17,13 +19,27 @@ class App extends Component {
         }
     }
     render() {
+        console.log('App render');
+        var _title, _desc = null;
+        if(this.state.mode === 'welcome') {
+            _title = this.state.welcome.title;
+            _desc = this.state.welcome.desc;
+        } else if(this.state.mode === 'read'){
+            _title = this.state.contents[0].title;
+            _desc = this.state.contents[0].desc;
+        }
         return (
             <div className="App">
                 <Subject
                     title={this.state.subject.title}
-                    sub={this.state.subject.sub}></Subject>
+                    sub={this.state.subject.sub}
+                    onChangePage={function(){
+                        this.setState({mode:'read'});
+                    }.bind(this)}
+                >
+                </Subject>
                 <TOC data={this.state.contents}></TOC>
-                <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+                <Content title={_title} desc={_desc}></Content>
             </div>
         );
     }
